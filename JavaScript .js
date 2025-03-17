1f9c4ae314981b265d22e6f0f2b61460ff937479
youcef22y <kikom5354@gmail.com>
2025-03-17 15:41:02 +0100

    Add files via upload

---
+++ JavaScript .js
@@ -0,0 +1,9 @@
+document.addEventListener("keydown", function(event) {
+    if (event.key === "F12" || (event.ctrlKey && event.shiftKey && event.key === "I")) {
+        event.preventDefault();
+    }
+});
+document.addEventListener("contextmenu", function(event) {
+    event.preventDefault();
+});
+var _0xabc1=["\x68\x65\x6C\x6C\x6F","\x6C\x6F\x67"];console[_0xabc1[1]](_0xabc1[0])
